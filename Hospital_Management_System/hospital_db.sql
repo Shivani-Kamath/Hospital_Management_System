dbe729c46d9bda56735cab069c9a4b588f7821d2
Shivani-Kamath <shivanikamath369@gmail.com>
2025-05-30 19:01:16 +0530

    Update hospital_db.sql

--- Hospital_Management_System/hospital_db.sql
+++ Hospital_Management_System/hospital_db.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE Patients (
+    PatientID INT PRIMARY KEY,
+    PatientName VARCHAR(100),
+    Age INT,
+    Gender VARCHAR(10),
+    AdmissionDate DATE,
+    Ailment VARCHAR(255),
+    AssignedDoctor VARCHAR(100)
+ );
